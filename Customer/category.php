8bae6a9c187b3131ecdacc1eda1001af7dfcad3f
YCLOO\USER <1201200793@student.mmu.edu.my>
2023-05-15 08:01:59 +0800

    edit category ui

--- Customer/category.php
+++ Customer/category.php
@@ -86,8 +86,110 @@
                                     </div><!-- End .product-body -->
                                 </div><!-- End .product -->
                             </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
-                            
+                            <div class="col-6 col-md-4 col-lg-4 col-xl-3">
+                                <div class="product">
+                                    <figure class="product-media">
+                                        <span class="product-label label-new">New</span> <!--<span class="product-label label-out">Out of stock</span>-->
+                                        <a href="product.php">
+                                            <img src="assets/images/products/product-1.jpg" alt="Product image" class="product-image">
+                                        </a>
+
+                                        <div class="product-action action-icon-top">
+                                            
+                                            <a href="popup/quickView.php" class="btn-product btn-quickview" title="Quick view"><span>quick view</span></a>
+                                            <a href="#" class="btn-product btn-compare" title="Compare"><span>compare</span></a>
+                                        </div><!-- End .product-action -->
+                                    </figure><!-- End .product-media -->
+
+                                    <div class="product-body">
+                                        <div class="product-cat">
+                                            <a href="#">Women</a>
+                                        </div><!-- End .product-cat -->
+                                        <h3 class="product-title"><a href="product.php">Nunc dignissim risus</a></h3><!-- End .product-title -->
+                                        <div class="product-price">
+                                            RM 50.00
+                                        </div><!-- End .product-price -->
+                                        
+                                        <!-- Color code--><!--
+                                        <div class="product-nav product-nav-dots">
+                                            <a href="#" style="background: #cc9966;"><span class="sr-only">Color name</span></a>
+                                            <a href="#" class="active" style="background: #ebebeb;"><span class="sr-only">Color name</span></a>
+                                        </div> End .product-nav -->
+
+                                    </div><!-- End .product-body -->
+                                </div><!-- End .product -->
+                            </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
+                            <div class="col-6 col-md-4 col-lg-4 col-xl-3">
+                                <div class="product">
+                                    <figure class="product-media">
+                                        <span class="product-label label-new">New</span> <!--<span class="product-label label-out">Out of stock</span>-->
+                                        <a href="product.php">
+                                            <img src="assets/images/products/product-1.jpg" alt="Product image" class="product-image">
+                                        </a>
+
+                                        <div class="product-action action-icon-top">
+                                            
+                                            <a href="popup/quickView.php" class="btn-product btn-quickview" title="Quick view"><span>quick view</span></a>
+                                            <a href="#" class="btn-product btn-compare" title="Compare"><span>compare</span></a>
+                                        </div><!-- End .product-action -->
+                                    </figure><!-- End .product-media -->
+
+                                    <div class="product-body">
+                                        <div class="product-cat">
+                                            <a href="#">Women</a>
+                                        </div><!-- End .product-cat -->
+                                        <h3 class="product-title"><a href="product.php">Nunc dignissim risus</a></h3><!-- End .product-title -->
+                                        <div class="product-price">
+                                            RM 50.00
+                                        </div><!-- End .product-price -->
+                                        
+                                        <!-- Color code--><!--
+                                        <div class="product-nav product-nav-dots">
+                                            <a href="#" style="background: #cc9966;"><span class="sr-only">Color name</span></a>
+                                            <a href="#" class="active" style="background: #ebebeb;"><span class="sr-only">Color name</span></a>
+                                        </div> End .product-nav -->
+
+                                    </div><!-- End .product-body -->
+                                </div><!-- End .product -->
+                            </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
+                            <div class="col-6 col-md-4 col-lg-4 col-xl-3">
+                                <div class="product">
+                                    <figure class="product-media">
+                                        <span class="product-label label-new">New</span> <!--<span class="product-label label-out">Out of stock</span>-->
+                                        <a href="product.php">
+                                            <img src="assets/images/products/product-1.jpg" alt="Product image" class="product-image">
+                                        </a>
+
+                                        <div class="product-action action-icon-top">
+                                            
+                                            <a href="popup/quickView.php" class="btn-product btn-quickview" title="Quick view"><span>quick view</span></a>
+                                            <a href="#" class="btn-product btn-compare" title="Compare"><span>compare</span></a>
+                                        </div><!-- End .product-action -->
+                                    </figure><!-- End .product-media -->
+
+                                    <div class="product-body">
+                                        <div class="product-cat">
+                                            <a href="#">Women</a>
+                                        </div><!-- End .product-cat -->
+                                        <h3 class="product-title"><a href="product.php">Nunc dignissim risus</a></h3><!-- End .product-title -->
+                                        <div class="product-price">
+                                            RM 50.00
+                                        </div><!-- End .product-price -->
+                                        
+                                        <!-- Color code--><!--
+                                        <div class="product-nav product-nav-dots">
+                                            <a href="#" style="background: #cc9966;"><span class="sr-only">Color name</span></a>
+                                            <a href="#" class="active" style="background: #ebebeb;"><span class="sr-only">Color name</span></a>
+                                        </div> End .product-nav -->
+
+                                    </div><!-- End .product-body -->
+                                </div><!-- End .product -->
+                            </div><!-- End .col-sm-6 col-lg-4 col-xl-3 -->
                         </div><!-- End .row -->
+
+                        
+                            
+                        
 
                         <div class="load-more-container text-center">
                             <a href="#" class="btn btn-outline-darker btn-load-more">More Products <i class="icon-refresh"></i></a>
