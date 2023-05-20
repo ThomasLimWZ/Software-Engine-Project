a5f59e16a1bcb9161e8245f854f3e6dfc72ec7f9
YCLOO2002\ycloo <ycloo2002@gmail.com>
2023-05-20 19:43:03 +0800

    temp

--- Customer/index.php
+++ Customer/index.php
@@ -11,7 +11,7 @@
             <div class="intro-section pt-3 pb-3 mb-2">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-8">
+                        <div class="col-lg-10 mx-auto">
                             <div class="intro-slider-container slider-container-ratio mb-2 mb-lg-0">
                                 <div class="intro-slider owl-carousel owl-simple owl-dark owl-nav-inside" data-toggle="owl" data-owl-options='{
                                         "nav": false, 
@@ -23,6 +23,7 @@
                                             }
                                         }
                                     }'>
+                                    <!-- for slide -->
                                     <div class="intro-slide">
                                         <figure class="slide-image">
                                             <picture>
@@ -84,45 +85,7 @@
                             </div><!-- End .intro-slider-container -->
                         </div><!-- End .col-lg-8 -->
 
-                        <div class="col-lg-4">
-                            <div class="intro-banners">
-                                <div class="banner mb-lg-1 mb-xl-2">
-                                    <a href="#">
-                                        <img src="assets/images/demos/demo-3/banners/banner-1.jpg" alt="Banner">
-                                    </a>
-
-                                    <div class="banner-content">
-                                        <h4 class="banner-subtitle d-lg-none d-xl-block"><a href="#">Top Product</a></h4><!-- End .banner-subtitle -->
-                                        <h3 class="banner-title"><a href="#">Edifier <br>Stereo Bluetooth</a></h3><!-- End .banner-title -->
-                                        <a href="#" class="banner-link">Shop Now<i class="icon-long-arrow-right"></i></a>
-                                    </div><!-- End .banner-content -->
-                                </div><!-- End .banner -->
-
-                                <div class="banner mb-lg-1 mb-xl-2">
-                                    <a href="#">
-                                        <img src="assets/images/demos/demo-3/banners/banner-2.jpg" alt="Banner">
-                                    </a>
-
-                                    <div class="banner-content">
-                                        <h4 class="banner-subtitle d-lg-none d-xl-block"><a href="#">Clearance</a></h4><!-- End .banner-subtitle -->
-                                        <h3 class="banner-title"><a href="#">GoPro - Fusion 360 <span>Save $70</span></a></h3><!-- End .banner-title -->
-                                        <a href="#" class="banner-link">Shop Now<i class="icon-long-arrow-right"></i></a>
-                                    </div><!-- End .banner-content -->
-                                </div><!-- End .banner -->
-
-                                <div class="banner mb-0">
-                                    <a href="#">
-                                        <img src="assets/images/demos/demo-3/banners/banner-3.jpg" alt="Banner">
-                                    </a>
-
-                                    <div class="banner-content">
-                                        <h4 class="banner-subtitle d-lg-none d-xl-block"><a href="#">Featured</a></h4><!-- End .banner-subtitle -->
-                                        <h3 class="banner-title"><a href="#">Apple Watch 4 <span>Our Hottest Deals</span></a></h3><!-- End .banner-title -->
-                                        <a href="#" class="banner-link">Shop Now<i class="icon-long-arrow-right"></i></a>
-                                    </div><!-- End .banner-content -->
-                                </div><!-- End .banner -->
-                            </div><!-- End .intro-banners -->
-                        </div><!-- End .col-lg-4 -->
+                        
                     </div><!-- End .row -->
                 </div><!-- End .container -->
             </div><!-- End .intro-section -->
@@ -141,6 +104,8 @@
                 </ul>
 
                 <div class="tab-content tab-content-carousel">
+
+                    <!-- for Featured -->
                     <div class="tab-pane p-0 fade show active" id="products-featured-tab" role="tabpanel" aria-labelledby="products-featured-link">
                         <div class="owl-carousel owl-full carousel-equal-height carousel-with-shadow" data-toggle="owl" 
                             data-owl-options='{
@@ -338,6 +303,8 @@
                             </div><!-- End .product -->
                         </div><!-- End .owl-carousel -->
                     </div><!-- .End .tab-pane -->
+
+                    <!-- for On Sale -->
                     <div class="tab-pane p-0 fade" id="products-sale-tab" role="tabpanel" aria-labelledby="products-sale-link">
                         <div class="owl-carousel owl-full carousel-equal-height carousel-with-shadow" data-toggle="owl" 
                             data-owl-options='{
@@ -502,6 +469,8 @@
                             </div><!-- End .product -->
                         </div><!-- End .owl-carousel -->
                     </div><!-- .End .tab-pane -->
+
+                    <!-- for Top Rated-->
                     <div class="tab-pane p-0 fade" id="products-top-tab" role="tabpanel" aria-labelledby="products-top-link">
                         <div class="owl-carousel owl-full carousel-equal-height carousel-with-shadow" data-toggle="owl" 
                             data-owl-options='{
