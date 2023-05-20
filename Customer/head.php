4ec4eb90a07aaab561f32f4589b38914a4cc4087
Thomas <weezheng@mysoftinn.com>
2023-05-20 22:42:38 +0800

    remove by suggestions

--- Customer/head.php
+++ Customer/head.php
@@ -6,28 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>4PEOPLE TELCO</title>
 
-    <!--  I think this can remove / change
-    <meta name="keywords" content="HTML5 Template">
-    <meta name="description" content="Molla - Bootstrap eCommerce Template">
-    <meta name="author" content="p-themes">
-    -->
-
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icons/4-peoples-telco-website-favicon-color.png">
-    
-    <!-- maybe can be delete
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icons/favicon-16x16.png">
-    -->
     
     <link rel="icon" type="image/png"  href="assets/images/icons/4-peoples-telco-website-favicon-color.png">
     <link rel="mask-icon" href="assets/images/icons/safari-pinned-tab.svg" color="#666666">
     <link rel="shortcut icon" href="assets/images/4-peoples-telco-website-favicon-color.png">
-
-    <!--  I think this can remove / change
-    <meta name="apple-mobile-web-app-title" content="Molla">
-    <meta name="application-name" content="Molla">
-    -->
 
     <meta name="msapplication-TileColor" content="#cc9966">
     <meta name="msapplication-config" content="assets/images/icons/browserconfig.xml">
