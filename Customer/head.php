67353f4e1d9c5e385501a49f69d61c1250bdd9e0
Thomas <weezheng@mysoftinn.com>
2023-05-20 22:50:43 +0800

    update

--- Customer/head.php
+++ Customer/head.php
@@ -1,5 +1,5 @@
-<?php session_start()?>
 <?php include("../Admin/connection.php");?>
+<?php session_start()?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- Customer/signout.php
+++ Customer/signout.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <body>
-<?php 
 
-    include('head.php'); 
+<?php 
+    include('head.php');
+
     unset($_SESSION["customer_id"]);
     session_destroy();
+
     echo "
-     <script>
-        Swal.fire(
-            'Log out Success!',
-            '',
-            'success'
-        ).then(() => window.location.href='index.php');
-    </script>
+        <script>
+            Swal.fire(
+                'Log out Success!',
+                '',
+                'success'
+            ).then(() => window.location.href='index.php');
+        </script>
     ";
-
 ?>
-
 
 </body>
 </html>
