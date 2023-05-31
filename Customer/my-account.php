411a1198e8fefe031ab18f5e6feadfd67b81bf10
YCLOO2002\ycloo <ycloo2002@gmail.com>
2023-05-31 13:38:13 +0800

    reset password done

--- Customer/my-account.php
+++ Customer/my-account.php
@@ -429,17 +429,94 @@ if (isset($_SESSION['customer_id'])) {
 								    </div><!-- .End .tab-pane -->
 
 								    <div class="tab-pane fade" id="tab-reset-password" role="tabpanel" aria-labelledby="tab-reset-password-link">
-								    	<form action="#">
-											<label>Current password (leave blank to leave unchanged)</label>
-		            						<input type="password" class="form-control">
+								    	<form action="#" method="post">
+											<label>Current password </label>
+		            						<input type="password" class="form-control" id="old_password" name="old_password" required>
+		            												
+											<label for="new_password">New password </label>
+											<input type="password" class="form-control" id="new_password" name="new_password" required
+												pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]){8,}"
+												title="Must contain number, uppercase, special characters and lowercase letter, and at least 8 or more characters">
+                                    
+											<div class="col-sm-12 mb-4 w-auto h-auto">
+												<div class="alert-warning px-4 py-3 mb-0 d-none" id="password-alert">
+													<ul class="list-unstyled mb-0">
+														<li class="requirements leng">
+															<i class="fas fa-check text-success me-2"></i>
+															<i class="fas fa-times text-danger me-3"></i>
+															&emsp;Your password must have at least 8 chars
+														</li>
+														<li class="requirements big-letter">
+															<i class="fas fa-check text-success me-2"></i>
+															<i class="fas fa-times text-danger me-3"></i>
+															&emsp;Your password must have at least 1 big letter.
+														</li>
+														<li class="requirements small-letter">
+															<i class="fas fa-check text-success me-2"></i>
+															<i class="fas fa-times text-danger me-3"></i>
+															&emsp;Your password must have at least 1 small letter.
+														</li>
+														<li class="requirements num">
+															<i class="fas fa-check text-success me-2"></i>
+															<i class="fas fa-times text-danger me-3"></i>
+															&emsp;Your password must have at least 1 number.
+														</li>
+														<li class="requirements special-char">
+															<i class="fas fa-check text-success me-2"></i>
+															<i class="fas fa-times text-danger me-3"></i>
+															&emsp;Your password must have at least 1 special char.
+														</li>
+													</ul>
+												</div>
+											</div>
+                                   
+                                        	<label for="new_comfirm_password">Confirm new password </label>
+                                        	<input type="password" class="form-control mb-2" id="new_comfirm_password" name="new_comfirm_password" 
+                                            	pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]){8,}"
+                                            	title="Must contain number, uppercase, special characters and lowercase letter, and at least 8 or more characters" required>
+												
+											<div class="col-sm-12 mb-4 w-auto h-auto">
+												<div class="alert-warning px-4 py-3 mb-0 d-none" id="confirm-password-alert">
+													<ul class="list-unstyled mb-0">
+														<li class="requirement same-pass">
+															<i class="fas fa-check text-success me-2"></i>
+															<i class="fas fa-times text-danger me-3"></i>
+															&emsp;Both password and confirm password must be same.
+														</li>
+													</ul>
+												</div>
+											</div>
+											<style>
+												.alert-warning {
+													color: #806520;
+													background-color: #fdf3d8;
+													border-color: #fceec9;
+												}
+												.alert-success {
+													color: #0f6848;
+													background-color: #d2f4e8;
+													border-color: #bff0de;
+												}
+												.wrong .fa-check {
+													display: none;
+												}
 
-		            						<label>New password (leave blank to leave unchanged)</label>
-		            						<input type="password" class="form-control">
+												.good .fa-times {
+													display: none;
+												}
+											</style>
+											<div class="form-group">
+												<div class="custom-control custom-checkbox">
+													<input type="checkbox" class="custom-control-input" id="show_password">
+													<label class="custom-control-label" for="show_password">Show Password</label>
+												</div><!-- End .custom-checkbox -->
+											</div><!-- End .form-group -->
+											<button type="button" class="btn btn-outline-primary-2" onclick="clear_all_value()" style="padding:12px 0px">
+			                					<span>Clear</span>
+												
+			                				</button>
 
-		            						<label>Confirm new password</label>
-		            						<input type="password" class="form-control mb-2">
-
-		                					<button type="submit" class="btn btn-outline-primary-2">
+		                					<button type="submit" class="btn btn-outline-primary-2" id="reset_password" name="reset_password">
 			                					<span>SAVE CHANGES</span>
 			            						<i class="icon-long-arrow-right"></i>
 			                				</button>
@@ -482,6 +559,229 @@ if (isset($_SESSION['customer_id'])) {
 			document.getElementById('edit_adress').style.display='block';
 
 		}
+		function clear_all_value()
+		{
+			document.getElementById("old_password").value="";
+			document.getElementById("new_password").value="";
+			document.getElementById("new_comfirm_password").value="";
+
+		}
+		let nshowPassword = document.querySelector("#show_password");
+		let oldPassword = document.querySelector("#old_password");
+		let newPassword = document.querySelector("#new_password");
+		let newConfirmPassword = document.querySelector('#new_comfirm_password');
+
+		nshowPassword.addEventListener("click", () => {
+			let type = oldPassword.getAttribute("type") === "password" && newPassword.getAttribute("type") === "password" && newConfirmPassword.getAttribute("type") === "password" ? "text" : "password";
+			oldPassword.setAttribute("type", type);
+			newPassword.setAttribute("type", type);
+			newConfirmPassword.setAttribute("type", type);
+		});
+
+		addEventListener("DOMContentLoaded", (event) => {
+			const password = document.getElementById("new_password");
+			const passwordAlert = document.getElementById("password-alert");
+			const requirements = document.querySelectorAll(".requirements");
+
+			let lengBoolean, bigLetterBoolean, smallLetterBoolean, numBoolean, specialCharBoolean;
+			let leng = document.querySelector(".leng");
+			let bigLetter = document.querySelector(".big-letter");
+			let smallLetter = document.querySelector(".small-letter");
+			let num = document.querySelector(".num");
+			let specialChar = document.querySelector(".special-char");
+
+			const specialChars = "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?`~";
+			const numbers = "0123456789";
+
+			requirements.forEach((element) => element.classList.add("wrong"));
+
+			password.addEventListener("focus", () => {
+				passwordAlert.classList.remove("d-none");
+
+				if (!password.classList.contains("is-valid")) {
+					password.classList.add("is-invalid");
+				}
+			});
+			
+			password.addEventListener("blur", () => {
+				passwordAlert.classList.add("d-none");
+			});
+
+			password.addEventListener("input", () => {
+				validationAction(password);
+			});
+
+			function validationAction(passwordType) {
+				let value = passwordType.value;
+
+				if (value.length < 8) {
+					lengBoolean = false;
+				} else if (value.length > 7) {
+					lengBoolean = true;
+				}
+
+				let lowerCaseLetters = /[a-z]/g;
+				if(value.match(lowerCaseLetters)) {  
+					smallLetterBoolean = true;
+				} else {
+					smallLetterBoolean = false;
+				}
+				
+				let upperCaseLetters = /[A-Z]/g;
+				if(value.match(upperCaseLetters)) {  
+					bigLetterBoolean = true;
+				} else {
+					bigLetterBoolean = false;
+				}
+
+				numBoolean = false;
+				for (let i = 0; i < value.length; i++) {
+					for (let j = 0; j < numbers.length; j++) {
+						if (value[i] == numbers[j]) {
+							numBoolean = true;
+						}
+					}
+				}
+
+				specialCharBoolean = false;
+				for (let i = 0; i < value.length; i++) {
+					for (let j = 0; j < specialChars.length; j++) {
+						if (value[i] == specialChars[j]) {
+							specialCharBoolean = true;
+						}
+					}
+				}
+
+				if (lengBoolean == true && bigLetterBoolean == true && smallLetterBoolean == true && numBoolean == true && specialCharBoolean == true) {
+					passwordType.classList.remove("is-invalid");
+					passwordType.classList.add("is-valid");
+
+					requirements.forEach((element) => {
+						element.classList.remove("wrong");
+						element.classList.add("good");
+					});
+					passwordAlert.classList.remove("alert-warning");
+					passwordAlert.classList.add("alert-success");
+				} else {
+					passwordType.classList.remove("is-valid");
+					passwordType.classList.add("is-invalid");
+
+					passwordAlert.classList.add("alert-warning");
+					passwordAlert.classList.remove("alert-success");
+
+					if (lengBoolean == false) {
+						leng.classList.add("wrong");
+						leng.classList.remove("good");
+					} else {
+						leng.classList.add("good");
+						leng.classList.remove("wrong");
+					}
+
+					if (bigLetterBoolean == false) {
+						bigLetter.classList.add("wrong");
+						bigLetter.classList.remove("good");
+					} else {
+						bigLetter.classList.add("good");
+						bigLetter.classList.remove("wrong");
+					}
+
+					if (smallLetterBoolean == false) {
+						smallLetter.classList.add("wrong");
+						smallLetter.classList.remove("good");
+					} else {
+						smallLetter.classList.add("good");
+						smallLetter.classList.remove("wrong");
+					}
+
+					if (numBoolean == false) {
+						num.classList.add("wrong");
+						num.classList.remove("good");
+					} else {
+						num.classList.add("good");
+						num.classList.remove("wrong");
+					}
+
+					if (specialCharBoolean == false) {
+						specialChar.classList.add("wrong");
+						specialChar.classList.remove("good");
+					} else {
+						specialChar.classList.add("good");
+						specialChar.classList.remove("wrong");
+					}
+				}
+			}
+
+			const confirmPassword = document.getElementById("new_comfirm_password");
+			const confirmPasswordAlert = document.getElementById("confirm-password-alert");
+			const requirement = document.querySelectorAll(".requirement");
+
+			let samePassBoolean;
+			let samePass = document.querySelector(".same-pass");
+
+			requirement.forEach((element) => element.classList.add("wrong"));
+
+			confirmPassword.addEventListener("focus", () => {
+				confirmPasswordAlert.classList.remove("d-none");
+
+				if (!confirmPassword.classList.contains("is-valid")) {
+					confirmPassword.classList.add("is-invalid");
+				}
+			});  
+
+			confirmPassword.addEventListener("blur", () => {
+				confirmPasswordAlert.classList.add("d-none");
+			});
+
+			$('#new_password, #new_comfirm_password').on('input', function () {
+				if ($('#new_password').val().length != 0 && $('#new_comfirm_password').val().length != 0) {
+					bothPasswordValidation();
+					return false;
+				} else if ($('#new_password').val().length != 0 && $('#new_comfirm_password').val().length != 0) {
+					bothPasswordValidation();
+					document.getElementById('reset_password').disabled = true;
+				} else {
+					document.getElementById('reset_password').disabled = true;
+				}
+			});
+
+			function bothPasswordValidation() {
+				if ($('#new_password').val() == $('#new_comfirm_password').val()) {
+					samePassBoolean = true;
+					if (lengBoolean == true && bigLetterBoolean == true && smallLetterBoolean == true && numBoolean == true && specialCharBoolean == true) {
+						document.getElementById('reset_password').disabled = false;
+					}
+				} else {
+					samePassBoolean = false;
+					document.getElementById('reset_password').disabled = true;
+				}
+
+				if (samePassBoolean == true) {
+					confirmPassword.classList.remove("is-invalid");
+					confirmPassword.classList.add("is-valid");
+
+					requirement.forEach((element) => {
+						element.classList.remove("wrong");
+						element.classList.add("good");
+					});
+					confirmPasswordAlert.classList.remove("alert-warning");
+					confirmPasswordAlert.classList.add("alert-success");
+				} else {
+					confirmPassword.classList.remove("is-valid");
+					confirmPassword.classList.add("is-invalid");
+
+					confirmPasswordAlert.classList.add("alert-warning");
+					confirmPasswordAlert.classList.remove("alert-success");
+
+					if (samePassBoolean == false) {
+						samePass.classList.add("wrong");
+						samePass.classList.remove("good");
+					} else {
+						samePass.classList.add("good");
+						samePass.classList.remove("wrong");
+					}
+				}
+			}
+		});
 	</script>
 </body>
 <?php
@@ -517,6 +817,74 @@ if(isset($_POST['submit_shipping']))
 		window.location.assign("my-account.php?goto=tab-address");
 	</script>
 	<?php
+}
+
+if(isset($_POST['reset_password']))
+{
+	$id = $_SESSION["customer_id"];
+	$old_pass = $_POST['old_password'];
+	$new_pass = $_POST['new_password'];
+	$con_new_pass = $_POST['new_comfirm_password'];
+	
+
+	$getCustomerQuery = mysqli_query($connect, "SELECT * FROM customer WHERE cus_id='$id' AND cus_pass = '".strtoupper(md5($old_pass))."'");
+    $countCustomer = mysqli_num_rows($getCustomerQuery);
+
+	if ($countCustomer == 1) {
+		
+		if ($new_pass === $con_new_pass) {
+			$customerRow = mysqli_fetch_assoc($getCustomerQuery);
+			mysqli_query($connect,"UPDATE `customer` SET cus_pass = '".strtoupper(md5($new_pass))."' WHERE cus_id = '$id' ");
+			
+			$email = $customerRow['cus_email'];
+			$subject = "Change Password Successful!";
+			$message = "Dear ".$name.",\n\nYour password has been change. \n\nRegards,\n4People Telco";
+			$headers = "From: 4People Telco" . "\r\n";
+			
+			if (mail($email, $subject, $message, $headers)) {
+				echo "
+					<script>
+						Swal.fire(
+							'Change successfull!',
+							'',
+							'success'
+						).then(() => window.location.assign('my-account.php'));	
+					</script>
+				";
+			}
+			else
+			{
+				?>
+				<script>
+					$('a[href="#<?php echo $go;?>"]').tab('show');
+				</script>
+				<?php
+			}
+		} else {
+			echo "
+				<script>
+					Swal.fire(
+						'Both password and confirm password not matched!',
+						'',
+						'warning'
+					).then(() => window.location.assign('my-account.php'));		
+				</script>
+			";
+		}
+	} else {
+		?>
+			<script>
+				Swal.fire(
+					'Fail to Reset Password',
+					'Wrong current password',
+					'warning'
+				).then(() => {
+					window.location.assign("my-account.php?goto=tab-reset-password");
+				});;
+			</script>
+		<?php
+	}
+
 }
 
 if(isset($_GET['goto']))
