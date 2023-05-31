dcc413d6ef9ad7cd6fce01b1ed3977a7ced860f7
YCLOO2002\ycloo <ycloo2002@gmail.com>
2023-05-31 12:32:55 +0800

    account detail&Address done

--- Customer/my-account.php
+++ Customer/my-account.php
@@ -81,6 +81,10 @@
 </style>
 <?php
 if (isset($_SESSION['customer_id'])) {
+	$cus_id = $_SESSION["customer_id"];
+    $result = mysqli_query($connect,"SELECT * FROM customer WHERE cus_id = '$cus_id'");
+    $cus_info = mysqli_fetch_assoc($result);
+	
 ?>
 <body>
     <div class="page-wrapper">
@@ -108,10 +112,10 @@ if (isset($_SESSION['customer_id'])) {
 	                		<aside class="col-md-4 col-lg-3">
 	                			<ul class="nav nav-dashboard flex-column mb-3 mb-md-0" role="tablist">
 								    <li class="nav-item">
-								        <a class="nav-link active" id="tab-orders-link" data-toggle="tab" href="#tab-orders" role="tab" aria-controls="tab-orders" aria-selected="false">Orders</a>
+								        <a class="nav-link active " id="tab-orders-link" data-toggle="tab" href="#tab-orders" role="tab" aria-controls="tab-orders" aria-selected="false">Orders</a>
 								    </li>
 								    <li class="nav-item">
-								        <a class="nav-link" id="tab-account-link" data-toggle="tab" href="#tab-account" role="tab" aria-controls="tab-account" aria-selected="false">Account Details</a>
+								        <a class="nav-link " id="tab-account-link" data-toggle="tab" href="#tab-account" role="tab" aria-controls="tab-account" aria-selected="false">Account Details</a>
 								    </li>
 								    <li class="nav-item">
 								        <a class="nav-link" id="tab-address-link" data-toggle="tab" href="#tab-address" role="tab" aria-controls="tab-address" aria-selected="false">Adresses</a>
@@ -316,59 +320,108 @@ if (isset($_SESSION['customer_id'])) {
 								    </div><!-- .End .tab-pane -->
 
 								    <div class="tab-pane fade" id="tab-address" role="tabpanel" aria-labelledby="tab-address-link">
-								    	<p>The following addresses will be used on the checkout page by default.</p>
+								    	<p id="show_shipping_word">The following addresses will be used on the checkout page by default.</p>
 
-								    	<div class="row">
+								    	<div class="row" id="show_shipping">
 								    		<div class="col-lg-6">
 								    			<div class="card card-dashboard">
-								    				<div class="card-body">
-								    					<h3 class="card-title">Billing Address</h3><!-- End .card-title -->
-
-														<p>User Name<br>
-														User Company<br>
-														John str<br>
-														New York, NY 10001<br>
-														1-234-987-6543<br>
-														yourmail@mail.com<br>
-														<a href="#">Edit <i class="icon-edit"></i></a></p>
-								    				</div><!-- End .card-body -->
-								    			</div><!-- End .card-dashboard -->
-								    		</div><!-- End .col-lg-6 -->
-
-								    		<div class="col-lg-6">
-								    			<div class="card card-dashboard">
-								    				<div class="card-body">
+								    				<div class="card-body" >
 								    					<h3 class="card-title">Shipping Address</h3><!-- End .card-title -->
 
-														<p>You have not set up this type of address yet.<br>
-														<a href="#">Edit <i class="icon-edit"></i></a></p>
+														<p>
+															<?php 
+																if(isset($cus_info['cus_address']))
+																	echo 
+																	$cus_info['cus_name'].'
+																	<br>
+																	'.$cus_info['cus_address'].' ,<br>
+																	'.$cus_info['cus_city'].' ,<br>
+																	'.$cus_info['cus_postcode'].' , '.$cus_info['cus_state'].'<br>
+																	'.$cus_info['cus_phone'].'<br>
+																	'.$cus_info['cus_email'].'<br>
+																	';
+																else
+																echo 'You have not set up this type of address yet.<br>';	
+															?>
+															<a href="#" onclick="open_close_address()">Edit <i class="icon-edit"></i></a>
+														</p>
 								    				</div><!-- End .card-body -->
+													
 								    			</div><!-- End .card-dashboard -->
 								    		</div><!-- End .col-lg-6 -->
+
+
 								    	</div><!-- End .row -->
+										<div style="display:none;" id="edit_adress">
+											<form action="#" method="POST" autocomplete="off">
+																							
+												<label> Name </label>
+												<input type="text" class="form-control" value="<?php echo $cus_info['cus_name']; ?>" readonly>
+
+												<label>Address <span class="text-danger">*</span></label>
+													<input type="text" class="form-control" value="<?php echo $cus_info['cus_address']; ?>" name="address_edit" placeholder="Your Address" required>
+
+												<label>City <span class="text-danger">*</span></label>												
+													<input type="test" class="form-control" value="<?php echo $cus_info['cus_city'];?>" name="city_edit" placeholder="Your City" required>
+												<div class="row">
+			                						<div class="col-sm-6">
+														<label>Postcode<span class="text-danger">*</span></label>
+															<input type="text" class="form-control" value="<?php echo $cus_info['cus_postcode'];?>" oninput="this.value = this.value.replace(/[^0-9.]/g, '');" pattern="[0-9]{5}" maxlength="5" 
+																placeholder="Your postcode" name="postcode_edit" required>
+													</div>
+
+													<div class="col-sm-6">
+													<label>State<span class="text-danger" >*</span></label>
+													<select class="form-control" name="state_edit" required><i class="fa fa-x"></i>
+														<option value="">Select State</option>
+														<option <?php if($cus_info['cus_state'] == "Melaka") echo "selected";?>>Melaka</option>
+														<option <?php if($cus_info['cus_state'] == "Johor") echo "selected";?>>Johor</option>
+														<option <?php if($cus_info['cus_state'] == "Selangor") echo "selected";?>>Selangor</option>
+														<option <?php if($cus_info['cus_state'] == "Negeri Sembilan") echo "selected";?>>Negeri Sembilan</option>
+														<option <?php if($cus_info['cus_state'] == "Pulau Pinang") echo "selected";?>>Pulau Pinang</option>
+														<option <?php if($cus_info['cus_state'] == "Kedah") echo "selected";?>>Kedah</option>
+														<option <?php if($cus_info['cus_state'] == "Kelantan") echo "selected";?>>Kelantan</option>
+														<option <?php if($cus_info['cus_state'] == "Pahang") echo "selected";?>>Pahang</option>
+														<option <?php if($cus_info['cus_state'] == "Perlis") echo "selected";?>>Perlis</option>
+														<option <?php if($cus_info['cus_state'] == "Perak") echo "selected";?>>Perak</option>
+														<option <?php if($cus_info['cus_state'] == "Sabah") echo "selected";?>>Sabah</option>
+														<option <?php if($cus_info['cus_state'] == "Sarawak") echo "selected";?>>Sarawak</option>
+														<option <?php if($cus_info['cus_state'] == "Terengganu") echo "selected";?>>Terengganu</option>
+													</select>
+													</div>
+
+												</div>
+
+												<label>Email Address </label>
+													<input type="email" class="form-control" value="<?php echo $cus_info['cus_email'];?>" readonly>
+
+												<label>Phone Number </label>
+													<input type="text" class="form-control" value="<?php echo $cus_info['cus_phone'];?>" readonly>
+
+												<button type="submit" class="btn btn-outline-primary-2" name="submit_shipping">
+													<span>SAVE CHANGES</span>
+													<i class="icon-long-arrow-right"></i>
+												</button>
+											</form>
+										</div>
 								    </div><!-- .End .tab-pane -->
 
 								    <div class="tab-pane fade" id="tab-account" role="tabpanel" aria-labelledby="tab-account-link">
-								    	<form action="#">
+								    	<form action="#" method="POST" autocomplete="off">
 			                				<div class="row">
 			                					<div class="col-sm-6">
-			                						<label>First Name <span class="text-danger">*</span></label>
-			                						<input type="text" class="form-control" required>
-			                					</div><!-- End .col-sm-6 -->
-
-			                					<div class="col-sm-6">
-			                						<label>Last Name <span class="text-danger">*</span></label>
-			                						<input type="text" class="form-control" required>
+			                						<label> Name <span class="text-danger">*</span></label>
+			                						<input type="text" class="form-control" value="<?php echo $cus_info['cus_name']; ?>" name="name_edit" required>
 			                					</div><!-- End .col-sm-6 -->
 			                				</div><!-- End .row -->
 
 		                					<label>Email Address <span class="text-danger">*</span></label>
-		        							<input type="email" class="form-control" required>
+		        							<input type="email" class="form-control" value="<?php echo $cus_info['cus_email'];?>" name="email_edit" readonly>
 
 											<label>Phone Number <span class="text-danger">*</span></label>
-											<input type="number" class="form-control" required>
+											<input type="text" class="form-control" value="<?php echo $cus_info['cus_phone'];?>" placeholder="01xxxxxxxx" min="10" max="11" name="phone_edit" required>
 
-		                					<button type="submit" class="btn btn-outline-primary-2">
+		                					<button type="submit" class="btn btn-outline-primary-2" name="submit_accountdetails">
 			                					<span>SAVE CHANGES</span>
 			            						<i class="icon-long-arrow-right"></i>
 			                				</button>
@@ -420,10 +473,61 @@ if (isset($_SESSION['customer_id'])) {
     <script src="assets/js/owl.carousel.min.js"></script>
     <!-- Main JS File -->
     <script src="assets/js/main.js"></script>
+	<script>
+		function open_close_address()
+		{
+			document.getElementById('show_shipping').style.display='none';
+			document.getElementById('show_shipping_word').style.display='none';
+
+			document.getElementById('edit_adress').style.display='block';
+
+		}
+	</script>
 </body>
 <?php
 } else {
 	header('Location: 404.php');
+}
+
+
+if(isset($_POST['submit_accountdetails']))
+{
+	$id = $_SESSION["customer_id"];
+	$name = $_POST['name_edit'];
+	$phone = $_POST['phone_edit'];
+
+	mysqli_query($connect,"UPDATE customer SET cus_name = '$name', cus_phone = '$phone' WHERE cus_id = '$id' ");
+	?>
+	<script>
+		window.location.assign("my-account.php?goto=tab-account");
+	</script>
+	<?php
+}
+
+if(isset($_POST['submit_shipping']))
+{
+	$id = $_SESSION["customer_id"];
+	$address = $_POST['address_edit'];
+	$city = $_POST['city_edit'];
+	$state = $_POST['state_edit'];
+	$postcode = $_POST['postcode_edit'];
+	mysqli_query($connect,"UPDATE `customer` SET `cus_address`='$address',`cus_city`='$city',`cus_state`='$state',`cus_postcode`='$postcode' WHERE cus_id = '$id' ");
+	?>
+	<script>
+		window.location.assign("my-account.php?goto=tab-address");
+	</script>
+	<?php
+}
+
+if(isset($_GET['goto']))
+{
+	$go = $_GET['goto'];
+	echo $go;
+	?>
+	<script>
+		$('a[href="#<?php echo $go;?>"]').tab('show');
+	</script>
+	<?php
 }
 ?>
 </html>
