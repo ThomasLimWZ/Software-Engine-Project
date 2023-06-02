7b883bdcf2b6ea0e6055ae3a6d567ffdccc5c5d5
Thomas <weezheng@mysoftinn.com>
2023-06-02 15:59:32 +0800

    order detail review completed

--- Customer/order-details.php
+++ Customer/order-details.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <?php include('head.php') ?>
+
 <?php
 if (isset($_SESSION['customer_id'])) {
 ?>
@@ -10,25 +11,21 @@ if (isset($_SESSION['customer_id'])) {
         <?php include('header.php') ?>
 
 		<?php
-			if(isset($_GET["details"])){
-				$pay_code = $_GET["code"];
+			if (isset($_GET["details"])) {
+				$orderId = $_GET["code"];
 
-				$result = mysqli_query($connect, "SELECT * FROM `order` WHERE order_id='$pay_code'");
+				$result = mysqli_query($connect, "SELECT * FROM `order` 
+												INNER JOIN payment ON `order`.order_id = payment.order_id
+												INNER JOIN shipping ON `order`.order_id = shipping.order_id
+												WHERE `order`.order_id = '$orderId'");
 				$row = mysqli_fetch_assoc($result);
-				
-				$ship_res = mysqli_query($connect,"SELECT * FROM shipping WHERE order_id='$pay_code'");
-				$ship_row = mysqli_fetch_assoc($ship_res);
-				
-				$pay_res = mysqli_query($connect,"SELECT * FROM payment WHERE order_id='$pay_code'");
-				$pay_row = mysqli_fetch_assoc($pay_res);
-
 			}
 		?>
 
         <main class="main">
         	<div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
         		<div class="container">
-        			<h1 class="page-title">My Account<span>Shop</span></h1>
+        			<h1 class="page-title">My Account</h1>
         		</div><!-- End .container -->
         	</div><!-- End .page-header -->
             <nav aria-label="breadcrumb" class="breadcrumb-nav mb-3">
@@ -36,7 +33,7 @@ if (isset($_SESSION['customer_id'])) {
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.php">Home</a></li>
                         <li class="breadcrumb-item"><a href="my-account.php">My Account</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Invoice #<?php echo $row['order_id'];?></li>
+                        <li class="breadcrumb-item active" aria-current="page">Invoice #<?php echo $row['invoice_number'];?></li>
                     </ol>
                 </div><!-- End .container -->
             </nav><!-- End .breadcrumb-nav -->
@@ -46,17 +43,16 @@ if (isset($_SESSION['customer_id'])) {
 					<div class="container">
 					
 						<label>Purchase Date Time:</label>
-						<h6><?php
-							#$date = Date("d-m-Y H:i:s");
-							#echo $date//date_format($date,"d-m-Y H:i:s");
-
-							$date = date_create($row['order_datetime']);
-							echo date_format($date,"d-m-Y H:i:s");
-						?></h6>
+						<h6>
+							<?php
+								$date = date_create($row['order_datetime']);
+								echo date_format($date, "d-m-Y H:i:s");
+							?>
+						</h6>
 						<label>Sold to:</label>
-						<h6><?php echo $ship_row['receiver_name'];?></h6>
-						<h6><?php echo $ship_row['receiver_phone'];?></h6>
-						<h6><?php echo $ship_row['address'];?>, <?php echo $ship_row['city'];?>, <?php echo $ship_row['state'];?>, <?php echo $ship_row['postcode'];?>, Malaysia.</h6>
+						<h6><?php echo $row['receiver_name'];?></h6>
+						<h6><?php echo $row['receiver_phone'];?></h6>
+						<h6><?php echo $row['address'];?>, <?php echo $row['city'];?>, <?php echo $row['state'];?>, <?php echo $row['postcode'];?>, Malaysia.</h6>
 						
 						<br>
 						<table style="margin:auto; border-collapse:separate; border:solid black 1px; border-radius:6px; font-size:12pt;">
@@ -72,52 +68,41 @@ if (isset($_SESSION['customer_id'])) {
 							<?php
 								$i = 1;
 								$prod_res = mysqli_query($connect,"SELECT * FROM cart_item 
-								JOIN product ON cart_item.prod_id=product.prod_id 
-								JOIN product_color ON product_color.prod_color_id=cart_item.prod_color_id 
-								WHERE order_id='$pay_code'");
-								while($prod_row = mysqli_fetch_assoc($prod_res)){
-									$detail = $prod_row['prod_detail_id'];
-									$pcode = $prod_row['prod_color_id'];
-									$detail_res = mysqli_query($connect,"SELECT * FROM product_detail WHERE prod_detail_id='$detail'");
-									$detail_row = mysqli_fetch_assoc($detail_res);
-									$img_res = mysqli_query($connect,"SELECT * FROM product_color WHERE prod_color_id='$pcode'");
-									$img_row = mysqli_fetch_assoc($img_res);
+																	INNER JOIN product ON cart_item.prod_id = product.prod_id 
+																	INNER JOIN product_detail ON cart_item.prod_detail_id = product_detail.prod_detail_id
+																	INNER JOIN product_color ON cart_item.prod_color_id = product_color.prod_color_id
+																	WHERE order_id = '$orderId'");
+
+								while ($prod_row = mysqli_fetch_assoc($prod_res)) {
 							?>
-							<tr>
-								<td style="text-align:center; padding:5px; border-top:solid black 1px; border-left: none;"><?php echo $i; ?></td>
-								<td style="padding:5px; border-left:solid black 1px; border-top:solid black 1px; text-align:left; display:flex; align-items:center;">
-									
-									<img src="../Product/<?php echo $img_row['prod_color_img']; ?>" width="100px" alt="product" style="float:left;">
-									
-									<div style="padding-left: 20px;"><?php echo $prod_row['prod_name']; ?></div>
-								</td>
-								<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;">
-								<?php echo $detail_row['prod_detail_name']; ?>
-								</td>
-								<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;"><?php echo $img_row['prod_color_name']; ?></td>
-								<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;"><?php echo $prod_row['quantity']; ?></td>
-								<td style="padding:5px; border-left:solid black 1px; border-top:solid black 1px;">RM <?php echo $detail_row['prod_detail_price']; ?></td>
-								<td style="padding:5px; border-left:solid black 1px; border-top:solid black 1px;">RM <?php echo $prod_row['cart_subtotal']; ?></td>
-							</tr>
+									<tr>
+										<td style="text-align:center; padding:5px; border-top:solid black 1px; border-left: none;"><?php echo $i; ?></td>
+										<td style="padding:5px; border-left:solid black 1px; border-top:solid black 1px; text-align:left; display:flex; align-items:center;">
+											
+											<img src="../Product/<?php echo $prod_row['prod_color_img']; ?>" width="100px" alt="product" style="float:left;">
+											
+											<div style="padding-left: 20px;"><?php echo $prod_row['prod_name']; ?></div>
+										</td>
+										<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;">
+										<?php echo empty($prod_row['prod_detail_name']) ? '-' : $prod_row['prod_detail_name']; ?>
+										</td>
+										<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;"><?php echo $prod_row['prod_color_name']; ?></td>
+										<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;"><?php echo $prod_row['quantity']; ?></td>
+										<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;">RM <?php echo $prod_row['prod_detail_price']; ?></td>
+										<td style="text-align:center; padding:5px; border-left:solid black 1px; border-top:solid black 1px;">RM <?php echo $prod_row['cart_subtotal']; ?></td>
+									</tr>
 							<?php
 									$i++;
 								}
 							?>	
-
 									<tr>
-										<td style="border-top:solid black 1px;"></td>
-										<td style="border-top:solid black 1px;"></td>
-										<td style="border-top:solid black 1px;"></td>
-										<td style="border-top:solid black 1px;"></td>
-										<td style="border-top:solid black 1px;"></td>
-										<td style="border-top:solid black 1px;"></td>
-										<td style="font-weight:bold; padding:5px; border-left:none; border-top:solid black 1px;">RM <?php echo $row['order_grandtotal']; ?></td>
+										<td class="text-right" colspan="7" style="font-weight:bold; padding:5px; border-left:none; border-top:solid black 1px;">RM <?php echo $row['order_grandtotal']; ?></td>
 									</tr>
 						</table>
 						
 						<br><br>
 						<a href="order-details-receipt.php?details&code=<?php echo $row['order_id'];?>&id=<?php echo $cus_id;?>" class="btn btn-primary" target="_blank"><i class="fas fa-file-pdf fa-lg"></i>Print PDF</a>&ensp;
-						<a href="my-account.php" class="btn btn-primary" >Back</a>
+						<a href="my-account.php" class="btn btn-primary">Back</a>
 						<br><br>
 					</div>
                 </div><!-- End .dashboard -->
